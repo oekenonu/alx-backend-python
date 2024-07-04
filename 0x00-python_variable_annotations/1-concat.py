0e141f59c5ae69418e33fd27c587001c8bf70a61
obinna E <obinnaekenonu@gmail.com>
2024-07-04 21:12:27 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+"""Uses annotations when adding two numbers"""
+
+
+def add(str1: str, str2: str) -> str:
+    """Returns the concat of 2 strings"""
+    return str1+str2
