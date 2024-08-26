bd3d24c4fb065d7adc75ff2ae9d6a024b4099ff2
oekenonu <146424918+oekenonu@users.noreply.github.com>
2024-08-26 19:47:19 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,6 +25,26 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
         self.assertEqual(context.exception.args[0], path[-1])
+
+
+class TestGetJson(unittest.TestCase):
+    """Testjson usecase test"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')  # Patch 'requests.get' in the correct module
+    def test_get_json(self, test_url, test_payload, mock_get):
+        # Create a Mock object with a json method that returns test_payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+
+        # Assert that the result is equal to the test_payload
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
